ca2fda796c1a4c12e0232d5daef37fee751b3803
Aya Abdelhamid <ayaa@andrew.cmu.edu>
2024-02-08 11:19:56 +0300

    added to git

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,10 +1,26 @@
     
-def convert(temp_fahrenheit)
-    # Convert Fahrenheit to Celsius using the formula (F - 32) * 5/9
-    temp_celsius = (temp_fahrenheit - 32) * 5.0 / 9.0
-    return temp_celsius
+
+def convert(temp, measure= "F")
+    return "Temperature must be an integer" unless temp.class == Integer
+    return "Temperature must be an integer" if temp.class != Integer
+    return "Temperature below Absolute Zero" if temp < -474 
+    if measure == "F"
+    return (temp-32) * 5/9 
+else
+    return (temp * 9/5) + 32
 end
+end
+    # Convert Fahrenheit to Celsius using the formula (F - 32) * 5/9
+    
+    
+    
       
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
+puts convert("zero")
+puts convert(-500)
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
